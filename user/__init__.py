089a0a6335a4eb113ea9f3ba70c2f108a53d8bd7
Kiril <kirilchyhsnchik@grmail.com>
2025-03-20 20:47:27 +0200

    Updated user, created class for it and admin

--- user/__init__.py
+++ user/__init__.py
@@ -1,1 +1,20 @@
 user = "ddd"
+status = "user"
+
+
+class User:
+    def __init__(self):
+        self.status = "user"
+
+    def add_post(self, text, title):
+        print("adding post")
+
+
+class Admin(User):
+    def __init__(self):
+        super().__init__(self)
+
+        self.status = "admin"
+    
+    def get_users(self) -> list[User]:
+        print(f"users: {[]}")
